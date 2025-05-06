1c945abe79a3f820000d08357ecc5f8d346b9531
Mustafa Bülbül <bulbulmustafa422@gmail.com>
2025-05-06 11:07:20 +0300

    add

--- parse/tokenize.c
+++ parse/tokenize.c
@@ -1,6 +1,8 @@
 #include "./../minishell.h"
 
 int	special_counter(int start, char *input);
+char	*edit_sep(char *sep);
+int	is_qote(char *input, int start, int i);
 char	**token_separate(char *input)
 {
 	char	**sep;
@@ -16,6 +18,7 @@ char	**token_separate(char *input)
 	{
 		len = special_counter(start, input);
 		sep[i] = ft_substr(input, start, len);
+		sep[i] = edit_sep(sep[i]);
 		start += len;
 		while (input[start] == ' ')
 			start++;
@@ -32,25 +35,8 @@ int	special_counter(int start, char *input)
 	i = 0;
 	while (input[start + i] == ' ')
 		i++;
-	if (input[start + i] == '"')
-	{
-		i++;
-		while (input[start + i] && input[start + i] != '"')
-			i++;
-		if (input[start + i] == '"')
-			i++;
-		return (i);
-	}
-	else if (input[start + i] == '\'')
-	{
-		i++;
-		while (input[start + i] && input[start + i] != '\'')
-			i++;
-		if (input[start + i] == '\'')
-			i++;
-		return (i);
-	}
-	else if (input[start + i] == '>')
+	i = is_qote(input, start, i);
+	if (input[start + i] == '>')
 	{
 		if (input[start + i + 1] == '>')
 			return (2);
@@ -68,11 +54,76 @@ int	special_counter(int start, char *input)
 	{
 		while (input[start + i] && input[start + i] != ' '
 			&& input[start + i] != '<' && input[start + i] != '>'
-			&& input[start + i] != '|' && input[start + i] != '"'
-			&& input[start + i] != '\'')
+			&& input[start + i] != '|')
 		{
+			i = is_qote(input, start, i);
 			i++;
 		}
 		return (i);
 	}
+	return (i);
+}
+
+int	is_qote(char *input, int start, int i)
+{
+	if (input[start + i] == '"')
+	{
+		i++;
+		while (input[start + i] && input[start + i] != '"')
+			i++;
+		if (input[start + i] == '"')
+			i++;
+	}
+	else if (input[start + i] == '\'')
+	{
+		i++;
+		while (input[start + i] && input[start + i] != '\'')
+			i++;
+		if (input[start + i] == '\'')
+			i++;
+	}
+	return (i);
+}
+
+char	*edit_sep(char *sep)
+{
+	int		s_qote;
+	int		d_qote;
+	int		i;
+	int		j;
+	char	*new_sep;
+
+	s_qote = 0;
+	d_qote = 0;
+	i = 0;
+	j = 0;
+	new_sep = (char *)malloc(sizeof(sep));
+	while (sep[i])
+	{
+		while (sep[i] == '"' && s_qote % 2 == 0)
+		{
+			d_qote++;
+			i++;
+			if (sep[i] == '"' && d_qote % 2 == 1)
+			{
+				i++;
+				d_qote++;
+			}
+		}
+		while (sep[i] == '\'' && d_qote % 2 == 0)
+		{
+			s_qote++;
+			i++;
+			if (sep[i] == '\'' && s_qote % 2 == 1)
+			{
+				i++;
+				s_qote++;
+			}
+		}
+		new_sep[j] = sep[i];
+		j++;
+		i++;
+	}
+	new_sep[j] = '\0';
+	return (new_sep);
 }
